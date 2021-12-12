957f9d32b3ec5066a316ab4402df928f11222115
Muhammad Nadeem <ghomnaam1234@gmail.com>
2021-12-11 23:56:12 -0800

    first commit

---
+++ server/app.js
@@ -0,0 +1,43 @@
+const dotenv = require("dotenv")
+const express =require ('express')
+const mongoose = require("mongoose")
+const app = express();
+dotenv.config({path: 'server/config.env'});
+
+require('./db/conn')
+
+app.use(express.json());
+//const User= require("./model/ProfileSchema")
+app.use(require('./Router/auth'));
+
+
+
+const PORT =process.env.PORT || 5000;
+
+
+
+/*app.get('/about' , ( req , res )=>{
+    res.send(`Hello from the About`);
+
+});*/
+
+if ( process.env.NODE_ENV == "production"){
+
+    app.use(express.static("client/build"));
+
+    const path = require("path");
+
+    app.get("*", (req, res) => {
+
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+    })
+
+
+}
+
+app.listen(PORT,()=>{
+    console.log(`Server is Running on port no ${PORT}`)
+})
+
+
